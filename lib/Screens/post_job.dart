d2060ec78200e200b807db034f08e524f86d3a63
prasaneet <prasaneet2000@gmail.com>
2020-12-18 23:05:50 +0530

    First Commit

--- lib/Screens/post_job.dart
+++ lib/Screens/post_job.dart
@@ -1,12 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:job_search/widgets/custom_appbar.dart';
+import 'package:job_search/widgets/custom_button.dart';
+import 'package:job_search/widgets/custom_textfield.dart';
 
 class PostJobPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Post Job  Page hello'),
-      ),
+        appBar: CustomAppBar(title: 'Post Job',isbackbutton: false,),
+        body: Padding(
+          padding: const EdgeInsets.only(top: 30,bottom: 20),
+          child: ListView(
+            children: [
+              CustomTextField(hinttext: 'Company Name',icon: Icons.account_balance,),
+              CustomTextField(hinttext: 'Role',icon: Icons.person,),
+              CustomTextField(hinttext: 'Skills',icon: Icons.create,),
+              CustomTextField(hinttext: 'Minimum Education',icon: Icons.collections_bookmark,),
+              CustomTextField(hinttext: 'Expected Salary',icon: Icons.attach_money,),
+              CustomTextField(hinttext: 'Job Location',icon: Icons.location_on,),
+              CustomTextField(hinttext: 'Working Hours',icon: Icons.timer,),
+              CustomTextField(hinttext: 'Last Date To apply',icon: Icons.calendar_today,),
+              CustomTextField(hinttext: 'Job Description',icon: Icons.description,),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: CustomButton(title: 'Post Job',isbackgroundcolor: true,),
+              )
+            ],
+          ),
+        )
     );
   }
 }
--- lib/Screens/user_account.dart
+++ lib/Screens/user_account.dart
@@ -1,11 +1,93 @@
 import 'package:flutter/material.dart';
+import 'package:job_search/widgets/custom_appbar.dart';
 class UserAccount extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('User Account Page'),
-      ),
+        appBar: CustomAppBar(title: 'Profile',isbackbutton: false,),
+        body: Padding(
+          padding: const EdgeInsets.only(top: 20,bottom: 10),
+          child: ListView(
+            children: [
+              GestureDetector(
+                onTap: (){},
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 5),
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 20,horizontal: 20),
+                    decoration: BoxDecoration(
+                        color: Colors.white
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text('Register Company',style: TextStyle(fontSize: 24,fontWeight: FontWeight.w600),),
+                        Icon(Icons.arrow_forward_ios)
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: (){},
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 5),
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 20,horizontal: 20),
+                    decoration: BoxDecoration(
+                        color: Colors.white
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text('Your Application',style: TextStyle(fontSize: 24,fontWeight: FontWeight.w600),),
+                        Icon(Icons.arrow_forward_ios)
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: (){},
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 5),
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 20,horizontal: 20),
+                    decoration: BoxDecoration(
+                        color: Colors.white
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text('Your Jobs Posted',style: TextStyle(fontSize: 24,fontWeight: FontWeight.w600),),
+                        Icon(Icons.arrow_forward_ios)
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: (){},
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 5),
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 20,horizontal: 20),
+                    decoration: BoxDecoration(
+                        color: Colors.white
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text('Your Jobs Applied',style: TextStyle(fontSize: 24,fontWeight: FontWeight.w600),),
+                        Icon(Icons.arrow_forward_ios)
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        )
     );
   }
 }
